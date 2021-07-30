0ee6352189d5925212389ef84efd3a6bfd26d9b5
ss <setrotec@outlook.com>
2021-07-30 16:45:30 -0300

    Lista V-for com V-if

---
+++ exemplos/lista-v-forComv-if/script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        colecaoCandidatos: [
+            {id: 1, nome: "Douglas", pontos: 550},
+            {id: 2, nome: "Albert", pontos: 530},
+            {id: 3, nome: "Carlos", pontos: 200},
+            {id: 4, nome: "Rafaela", pontos: 100},
+            {id: 5, nome: "Vera", pontos: 85}
+        ],
+    }
+});
